a2e3cbdd3cf37cb8d5acf922f09d89c0d5742d88
JacobBrunsting <jacob.brunsting@gmail.com>
2017-04-17 22:25:46 -0400

    Add animation to expand headers when scrolling to them

--- scripts.js
+++ scripts.js
@@ -1,1 +1,24 @@
-console.log("Hello World!");
+let expandingTexts = document.getElementsByClassName('expand-on-scroll');
+for (let i = 0; i < expandingTexts.length; ++i) {
+    let element = expandingTexts[i];
+    let textAlign = window.getComputedStyle(element).textAlign;
+    if (textAlign === 'center') {
+        element.style.transformOrigin = "50% 50%";
+    } else {
+        element.style.transformOrigin = "0 50%";
+    }
+    window.addEventListener("scroll", function(event) {        
+        let elementBoundingRect = element.getBoundingClientRect();
+        let elementDistToTop = elementBoundingRect.top;
+        let elementHeight = elementBoundingRect.height;
+        let elementPercentScrolled = elementDistToTop / window.innerHeight;
+        let percentToExpand = ((0.5 - Math.abs(elementPercentScrolled - 0.2)) - 0.3) * 0.6;
+        if (percentToExpand < 0) {
+            percentToExpand = 0;
+        } else if (percentToExpand > 0.08) {
+            percentToExpand = 0.08;
+        }
+        let scale = 1 + percentToExpand;
+        element.style.transform = "scale(" + scale + ", " + scale + ")";
+    });
+}
